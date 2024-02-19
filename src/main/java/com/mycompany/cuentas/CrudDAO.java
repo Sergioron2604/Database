161328f740ef93fa5030b043131f84210df878b2
Sergio Roncancio <sfroncancio71@ucatolica.edu.co>
2024-02-18 19:14:49 -0500

    Se sube el siguiente paquete

---
+++ src/main/java/com/mycompany/cuentas/CrudDAO.java
@@ -0,0 +1,10 @@
+package com.company.cuentas;
+import java.util.List;
+
+public interface CrudDAO<T> {
+    void save(T obj);
+    void update(T obj);
+    void delete(String primaryKey);
+    List<T> getAll();
+}
+
