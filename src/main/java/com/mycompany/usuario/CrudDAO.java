0cb75a5e4bd0cc170935bca1b086cda226e4f281
Sergioron2604 <132676171+Sergioron2604@users.noreply.github.com>
2024-02-25 01:23:39 -0500

    Delete src/main/java/com/mycompany directory
    Se ellimina archivos mal escalados

--- src/main/java/com/mycompany/usuario/CrudDAO.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.usuario;
-import java.util.List;
-
-public interface CrudDAO<T> {
-    void save(T obj);
-    void update(T obj);
-    void delete(String primaryKey);
-    List<T> getAll();
-}
